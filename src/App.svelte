7f7d33b002cbae95d184d41fb00f75f2efadf125
Henrique Lauro Bagio de Souza <henrique.souza@meltwater.com>
2021-10-22 15:25:40 -0300

    Adds example of notification and icon text change with a background service worker

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,22 @@
+<style>
+</style>
+
+<button id="test">Notify me {name}! {count}</button>
+
 <script>
   let count = 0;
+  export let name;
   function sum() {
     count++;
   }
+  
+  
+  //Sending notifications through browser.
+  window.addEventListener('load', function () {
+    const button = document.getElementById('test');
+    button.addEventListener('click', function () {
+      sum();
+      new Notification("Hi! " + count);
+    });
+  });
 </script>
-
-<p>Counter: {count}</p>
-<button on:click={sum}> + </button>
-
-<style>
-</style>
